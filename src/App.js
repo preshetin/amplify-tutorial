32044152c9d1f2daf9b7dc54b05a7cc299d0a957
Petr Reshetin <preshetin@gmail.com>
2019-05-15 13:43:11 +0300

    API add

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Amplify from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 Amplify.configure(awsmobile);
 
 function App() {
+
+  const post = async () => {
+    console.log('calling api');
+    const response = await API.post('myFirstAwsApiInAmplify', '/items', {
+      body: {
+        title: '1',
+        description: 'hello amplify!'
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  }
+
+  const get = async () => {
+    console.log('calling api');
+    const response = await API.get('myFirstAwsApiInAmplify', '/items/object/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+
+  const list = async () => {
+    console.log('calling api');
+    const response = await API.get('myFirstAwsApiInAmplify', '/items/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          11Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <p>api</p>
+        <button onClick={post}>POST</button>
+        <button onClick={get}>GET</button>
+        <button onClick={list}>LIST</button>
+      </div>
   );
 }
 
